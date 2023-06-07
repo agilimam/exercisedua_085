82546d923c6bb1c6208f2e3753e7d4c7e7a62198
agilimam <agilimamalhafizi04@gmail.com>
2023-06-07 09:09:36 +0700

    Add project files.

---
+++ exercisepemrograman2/exercisepemrograman2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+class bidangDatar {
+private:
+	int x; // variabel untuk menyimpan input dari lingkaran maupun bujur sangkar
+public:
+	bidangDatar() { // constructor
+
+	}
+};
