30e272ed7f4aa20ab27f12bf5e17bc56c546efca
agilimam <agilimamalhafizi04@gmail.com>
2023-06-07 10:25:03 +0700

    memanggil program utama

--- exercisepemrograman2/exercisepemrograman2.cpp
+++ exercisepemrograman2/exercisepemrograman2.cpp
@@ -1,11 +1,59 @@
 #include<iostream>
 using namespace std;
 
+class lingkaran {
+public:
+	void  setluas(int x);
+	void setkeliling(int x);
+};
+
+class bujurSangkar {
+public:
+	void setluas(int x);
+	void setkeliling(int x);
+		
+};
+
+
 class bidangDatar {
 private:
-	int x; // variabel untuk menyimpan input dari lingkaran maupun bujur sangkar
+	int x;
 public:
-	bidangDatar() { // constructor
+	bidangDatar() {
+
+		x = 0;
+	}
+};
+
+virtual void input(){
+
+	virtual float Luas(int a) {
+
+		return 0;
+	}
+	virtual float keliling(int a) {
+		return 0;
+	}
+	void setX(int a) {
+		this->x = a;
+	}
+	int getX() {
+		return x;
 
 	}
 };
+
+class Lingkaran :public bidangDatar {
+
+
+};
+
+class BujurSangkar :public bidangDatar {
+
+};
+
+
+
+int main() {
+
+}
